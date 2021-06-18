10eabf7b6d18a890ae27bdf3f348652cbb7f8720
ingaku <ingaku@slack-corp.com>
2021-06-18 15:57:08 -0400

    wrote api to get all data from products collection

--- controllers/products.js
+++ controllers/products.js
@@ -1,33 +1,26 @@
-const express = require('express')
-let router = express.Router()
+const express = require("express");
+let router = express.Router();
+
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Getting all
-router.get('/', (req, res) => {
-    res.json({
-        status: 'cool',
-        message: 'hello'
-    })
-})
-
+router.get("/", (req, res) => {
+  client.connect().then((client) => {
+    var db = client.db();
+    db.collection('products').find()
+      .toArray()
+      .then((data) => res.json(data))
+      .catch((e) => console.error(e));
+  });
+});
 
 // Getting one
-router.get('/:id', (req, res) => {
-    console.log(req.params.id)
-})
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+});
 
-// Creating one
-router.post('/', (req, res) => {
-    
-})
-
-// Updating one
-router.patch('/', (req, res) => {
-    
-})
-
-// Deleting one
-router.delete('/', (req, res) => {
-    
-})
-
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -3,22 +3,10 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-var db = require("./db");
-
 app.engine("jade", require("jade").__express);
 app.set("view engine", "jade");
 
 const productsRouter = require("./controllers/products");
 app.use("/api/products", productsRouter);
 
-// Connect to Mongo on start
-db.connect((err) => {
-  if (err) {
-    console.log("Unable to connect to mongo");
-    process.exit(1);
-  } else {
-    app.listen(3000, function () {
-      console.log("Listening on port 3000...");
-    });
-  }
-});
+app.listen(3000, () => console.log('started listening'))
