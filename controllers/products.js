afefbf756f4fa26e702c4a77da3fe27132a3ebbf
ingaku <ingaku@slack-corp.com>
2021-06-18 16:54:22 -0400

    wrote api for products/ and products/:id routes

--- controllers/products.js
+++ controllers/products.js
@@ -1,26 +1,42 @@
 const express = require("express");
+
 let router = express.Router();
 
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectID } = require('mongodb');
+
 const client = new MongoClient(process.env.DATABASE_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+main = async () => {
+  return client
+    .connect()
+    .then((myclient) => {
+      return myclient.db().collection("products");
+    })
+    .catch((e) => console.error(e))
+    .finally(client.close());
+};
+
 // Getting all
-router.get("/", (req, res) => {
-  client.connect().then((client) => {
-    var db = client.db();
-    db.collection('products').find()
-      .toArray()
-      .then((data) => res.json(data))
-      .catch((e) => console.error(e));
-  });
+router.get("/", async (req, res) => {
+  const products = await main().catch(console.error);
+  products
+    .find()
+    .toArray()
+    .then((data) => res.json(data))
+    .catch((e) => console.error(e));
 });
 
 // Getting one
-router.get("/:id", (req, res) => {
-  console.log(req.params.id);
+router.get("/:id", async (req, res) => {
+  const products = await main().catch(console.error);
+  products
+    .find({ _id: ObjectID(req.params.id) })
+    .toArray()
+    .then((data) => res.json(data))
+    .catch((e) => console.error(e));
 });
 
 module.exports = router;
